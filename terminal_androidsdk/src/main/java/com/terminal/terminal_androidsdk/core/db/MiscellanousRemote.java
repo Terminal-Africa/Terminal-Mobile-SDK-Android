47554b60146d64027cef68ddd89ca4a36aea9446
ayodejiolalekan <olalekanayodeji79@gmail.com>
2022-11-01 10:02:17 +0100

    Implementation containing the Fixes of all points shared on slack and other module testing.

--- terminal_androidsdk/src/main/java/com/terminal/terminal_androidsdk/core/db/MiscellanousRemote.java
+++ terminal_androidsdk/src/main/java/com/terminal/terminal_androidsdk/core/db/MiscellanousRemote.java
@@ -2,13 +2,10 @@ package com.terminal.terminal_androidsdk.core.db;
 
 import androidx.annotation.NonNull;
 
+import com.terminal.terminal_androidsdk.core.ITerminalConfiguration;
+import com.terminal.terminal_androidsdk.core.model.GetPackagingList;
 import com.terminal.terminal_androidsdk.core.network.BaseData;
 import com.terminal.terminal_androidsdk.core.network.RetrofitClientInstance;
-import com.terminal.terminal_androidsdk.core.ITerminalCities;
-import com.terminal.terminal_androidsdk.core.ITerminalCountries;
-import com.terminal.terminal_androidsdk.core.ITerminalPackaging;
-import com.terminal.terminal_androidsdk.core.ITerminalPackagingList;
-import com.terminal.terminal_androidsdk.core.ITerminalStates;
 import com.terminal.terminal_androidsdk.core.model.Packaging;
 import com.terminal.terminal_androidsdk.core.model.PackagingResponse;
 import com.terminal.terminal_androidsdk.core.model.TerminalCities;
@@ -43,14 +40,14 @@ public class MiscellanousRemote {
      *
      * All Countries , City, State,  End-point
      */
-    public void getCountries(@Nullable ITerminalCountries terminalConfig) {
+    public void getCountries(@Nullable ITerminalConfiguration<List<TerminalCountries>> terminalConfig) {
         RetrofitClientInstance.getInstance().getDataService().getCountries().enqueue(new Callback<BaseData<List<TerminalCountries>>>() {
             @Override
             public void onResponse(@NonNull Call<BaseData<List<TerminalCountries>>> call, @NonNull Response<BaseData<List<TerminalCountries>>> response) {
                 AppLog.d(LOG_TAG,"getCountries" + response);
 
                 if (response.isSuccessful()) {
-                    Objects.requireNonNull(terminalConfig).onResponse(Objects.requireNonNull(response.body()));
+                    Objects.requireNonNull(terminalConfig).onResponse(Objects.requireNonNull(response.body().getData()));
                 }
                 else {
                     if (terminalConfig != null) {
@@ -62,19 +59,18 @@ public class MiscellanousRemote {
             public void onFailure(@NonNull Call<BaseData<List<TerminalCountries>>> call, @NonNull Throwable t) {
                 AppLog.d(LOG_TAG,"getCountries" + t.getMessage());
                 Objects.requireNonNull(terminalConfig).onError(false, Objects.requireNonNull(t.getMessage()));
-
             }
         });
     }
 
-    public void getStatesInCountry(@Nullable ITerminalStates terminalConfig, String countryCode) {
+    public void getStatesInCountry(@Nullable  ITerminalConfiguration<List<TerminalStates>> terminalConfig, String countryCode) {
         RetrofitClientInstance.getInstance().getDataService().getStateInCountry(countryCode).enqueue(new Callback<BaseData<List<TerminalStates>>>() {
             @Override
             public void onResponse(@NonNull Call<BaseData<List<TerminalStates>>> call, @NonNull Response<BaseData<List<TerminalStates>>> response) {
                 AppLog.d(LOG_TAG,"getStatesInCountry" + response);
 
                 if (response.isSuccessful()) {
-                    Objects.requireNonNull(terminalConfig).onResponse(Objects.requireNonNull(response.body()));
+                    Objects.requireNonNull(terminalConfig).onResponse(Objects.requireNonNull(response.body().getData()));
                 }
                 else {
                     if (terminalConfig != null) {
@@ -91,14 +87,14 @@ public class MiscellanousRemote {
         });
     }
 
-    public void getCitiesInCountry(@Nullable ITerminalCities terminalConfig, String countryCode, String stateCode) {
+    public void getCitiesInCountry(@Nullable  ITerminalConfiguration<List<TerminalCities>> terminalConfig, String countryCode, String stateCode) {
         RetrofitClientInstance.getInstance().getDataService().getCitiesInState(countryCode,stateCode).enqueue(new Callback<BaseData<List<TerminalCities>>>() {
             @Override
             public void onResponse(@NonNull Call<BaseData<List<TerminalCities>>> call, @NonNull Response<BaseData<List<TerminalCities>>> response) {
                 AppLog.d(LOG_TAG,"getCitiesInCountry" + response);
 
                 if (response.isSuccessful()) {
-                    Objects.requireNonNull(terminalConfig).onResponse(Objects.requireNonNull(response.body()));
+                    Objects.requireNonNull(terminalConfig).onResponse(Objects.requireNonNull(response.body().getData()));
                 }
                 else {
                     if (terminalConfig != null) {
@@ -119,14 +115,14 @@ public class MiscellanousRemote {
      * All Packaging End-point
      */
 
-    public void createPackaging(@Nullable ITerminalPackaging terminalConfig, Packaging packaging) {
+    public void createPackaging(@Nullable ITerminalConfiguration<PackagingResponse> terminalConfig, Packaging packaging) {
         RetrofitClientInstance.getInstance().getDataService().createPackaging(packaging).enqueue(new Callback<BaseData<PackagingResponse>>() {
             @Override
             public void onResponse(@NonNull Call<BaseData<PackagingResponse>> call, @NonNull Response<BaseData<PackagingResponse>> response) {
                 AppLog.d(LOG_TAG,"createPackaging" + response);
 
                 if (response.isSuccessful()) {
-                    Objects.requireNonNull(terminalConfig).onResponse(Objects.requireNonNull(response.body()));
+                    Objects.requireNonNull(terminalConfig).onResponse(Objects.requireNonNull(response.body().getData()));
                 }
                 else {
                     if (terminalConfig != null) {
@@ -143,14 +139,14 @@ public class MiscellanousRemote {
         });
     }
 
-    public void deletePackaging(ITerminalPackaging terminalConfig, String packagingId) {
+    public void deletePackaging(ITerminalConfiguration<PackagingResponse> terminalConfig, String packagingId) {
         RetrofitClientInstance.getInstance().getDataService().deletePackaging(packagingId).enqueue(new Callback<BaseData<PackagingResponse>>() {
             @Override
             public void onResponse(@NonNull Call<BaseData<PackagingResponse>> call, @NonNull Response<BaseData<PackagingResponse>> response) {
                 AppLog.d(LOG_TAG,"deletePackaging" + response);
 
                 if (response.isSuccessful()) {
-                    terminalConfig.onResponse(Objects.requireNonNull(response.body()));
+                    terminalConfig.onResponse(Objects.requireNonNull(response.body().getData()));
                 } else {
                     BaseData errorResponse = Constant.INSTANCE.getBaseError(response);
                     terminalConfig.onError(errorResponse.isError(),errorResponse.getMessage());                }
@@ -164,14 +160,14 @@ public class MiscellanousRemote {
         });
     }
 
-    public void updatePackaging(ITerminalPackaging terminalConfig, String packagingId,Packaging packaging) {
+    public void updatePackaging(ITerminalConfiguration<PackagingResponse> terminalConfig, String packagingId,Packaging packaging) {
         RetrofitClientInstance.getInstance().getDataService().updatePackaging(packagingId,packaging).enqueue(new Callback<BaseData<PackagingResponse>>() {
             @Override
             public void onResponse(@NonNull Call<BaseData<PackagingResponse>> call, @NonNull Response<BaseData<PackagingResponse>> response) {
                 AppLog.d(LOG_TAG,"updatePackaging" + response);
 
                 if (response.isSuccessful()) {
-                    terminalConfig.onResponse(Objects.requireNonNull(response.body()));
+                    terminalConfig.onResponse(Objects.requireNonNull(Objects.requireNonNull(response.body()).getData()));
                 } else {
                     BaseData errorResponse = Constant.INSTANCE.getBaseError(response);
                     terminalConfig.onError(errorResponse.isError(),errorResponse.getMessage());                }
@@ -184,34 +180,33 @@ public class MiscellanousRemote {
         });
     }
 
-    public void getPackaging(ITerminalPackagingList terminalConfig) {
-        RetrofitClientInstance.getInstance().getDataService().getPackaging().enqueue(new Callback<BaseData<List<PackagingResponse>>>() {
+    public void getPackaging( ITerminalConfiguration<GetPackagingList> terminalConfig) {
+        RetrofitClientInstance.getInstance().getDataService().getPackaging().enqueue(new Callback<BaseData<GetPackagingList>>() {
             @Override
-            public void onResponse(@NonNull Call<BaseData<List<PackagingResponse>>> call, @NonNull Response<BaseData<List<PackagingResponse>>> response) {
+            public void onResponse(@NonNull Call<BaseData<GetPackagingList>> call, @NonNull Response<BaseData<GetPackagingList>> response) {
                 AppLog.d(LOG_TAG,"getPackaging" + response);
-
                 if (response.isSuccessful()) {
-                    terminalConfig.onResponse(Objects.requireNonNull(response.body()));
+                    terminalConfig.onResponse(Objects.requireNonNull(Objects.requireNonNull(response.body()).getData()));
                 } else {
                     BaseData errorResponse = Constant.INSTANCE.getBaseError(response);
-                    terminalConfig.onError(errorResponse.isError(),errorResponse.getMessage());                }
+                    terminalConfig.onError(Objects.requireNonNull(errorResponse).isError(),errorResponse.getMessage());                }
             }
             @Override
-            public void onFailure(@NonNull Call<BaseData<List<PackagingResponse>>> call, @NonNull Throwable t) {
+            public void onFailure(@NonNull Call<BaseData<GetPackagingList>> call, @NonNull Throwable t) {
                 AppLog.d(LOG_TAG,"getPackaging" + t.getMessage());
                 terminalConfig.onError(false, Objects.requireNonNull(t.getMessage()));
             }
         });
     }
 
-    public void getSpecificPackaging(ITerminalPackaging terminalConfig, String packagingId) {
+    public void getSpecificPackaging(ITerminalConfiguration<PackagingResponse> terminalConfig, String packagingId) {
         RetrofitClientInstance.getInstance().getDataService().getSpecificPackaging(packagingId).enqueue(new Callback<BaseData<PackagingResponse>>() {
             @Override
             public void onResponse(@NonNull Call<BaseData<PackagingResponse>> call, @NonNull Response<BaseData<PackagingResponse>> response) {
                 AppLog.d(LOG_TAG,"getSpecificPackaging" + response);
 
                 if (response.isSuccessful()) {
-                    terminalConfig.onResponse(Objects.requireNonNull(response.body()));
+                    terminalConfig.onResponse(Objects.requireNonNull(response.body().getData()));
                 } else {
                     BaseData errorResponse = Constant.INSTANCE.getBaseError(response);
                     terminalConfig.onError(errorResponse.isError(),errorResponse.getMessage());                }
